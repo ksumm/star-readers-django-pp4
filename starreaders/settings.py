39d3dae96f47c1a0d6c49fc4d3978d4c2db4b4dc
Oksana Mashyntseva <kmashintseva@gmail.com>
2023-08-07 08:17:54 +0000

    Create base template

--- starreaders/settings.py
+++ starreaders/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = 'starreaders.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
