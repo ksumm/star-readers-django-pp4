7b405e0773810be4dbc2f1dc42108a20bc742c0e
Oksana Mashyntseva <kmashintseva@gmail.com>
2023-10-03 13:20:27 +0000

    Change path

--- starreaders/settings.py
+++ starreaders/settings.py
@@ -163,7 +163,7 @@ LOGIN_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
